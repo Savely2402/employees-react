26bd0bab52120baa1b8fe9f25d2ccfbbce23380d
Savely <savely.jadan@gmail.com>
2024-10-04 19:04:58 +0700

    Add login and registration functionality with Prisma and jsonWebToken.

--- employees-server/controllers/users.js
+++ employees-server/controllers/users.js
@@ -1,9 +1,105 @@
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const prisma = new PrismaClient()
+
+/**
+ *
+ * @route POST api/user/login
+ * @desc Вход
+ * @access Public
+ */
+
 export const login = async (req, res) => {
-    res.send('login')
+    const { email, password } = req.body
+
+    console.log(email, password)
+
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ message: 'Пожалуйста, заполните обязательные поля' })
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email,
+        },
+    })
+
+    const isPasswordCorrect =
+        user && (await bcrypt.compare(password, user.password))
+
+    const secret = process.env.JWT_SECRET
+
+    if (user && isPasswordCorrect && secret) {
+        res.status(200).json({
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' }),
+        })
+    } else {
+        return res
+            .status(400)
+            .json({ message: 'Неверно введен логин или пароль' })
+    }
 }
 
+/**
+ *
+ * @route POST api/user/register
+ * @desc Регистрация
+ * @access Public
+ */
+
 export const register = async (req, res) => {
-    res.send('register')
+    const { email, password, name } = req.body
+
+    if (!email || !password || !name) {
+        return res
+            .status(400)
+            .json({ message: 'Пожалуйста, заполните обязательные поля' })
+    }
+
+    const registeredUser = await prisma.user.findFirst({
+        where: {
+            email,
+        },
+    })
+
+    if (registeredUser) {
+        return res
+            .status(400)
+            .json({ message: 'Пользователь с таким email уже существует' })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassword,
+        },
+    })
+
+    const secret = process.env.JWT_SECRET
+
+    if (user && secret) {
+        res.status(201).json({
+            id: user.id,
+            email: user.email,
+            name,
+            token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' }),
+        })
+    } else {
+        return res
+            .status(400)
+            .json({ message: 'Не удалось создать пользователя' })
+    }
 }
 
 export const current = async (req, res) => {
